08e4b5d7f2829b87517fe3da9c79df043c1eedd4
siddharthgupta441 <sidharthgupta441@gmail.com>
2025-08-01 10:30:24 +0530

    some changes

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-# import joblib
-# import streamlit as st
-# import joblib
-# import numpy as np
-# from data_training import get_embedding
+import joblib
+import streamlit as st
+import joblib
+import numpy as np
+from data_training import get_embedding
 
-# joblib.dump('stock_prediction_agent/model/clf.pkl')
+joblib.dump('stock_prediction_agent/model/clf.pkl')
 
-# model = joblib.load("model/clf.pkl")
-from yaspin import yaspin
-import time
-
-with yaspin(text="Loading...", color="cyan") as spinner:
-    time.sleep(5)  # simulate loading
-    spinner.ok("✅ ") 
+model = joblib.load("model/clf.pkl")
+ #in developing phase
